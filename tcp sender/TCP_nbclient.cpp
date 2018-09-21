ce1306135bd254260d62f231c35cfdec57a16293
ffoska <35177088+ffoska@users.noreply.github.com>
2018-09-21 18:28:42 +0200

    Add files via upload

--- tcp sender/TCP_nbclient.cpp
+++ tcp sender/TCP_nbclient.cpp
@@ -11,91 +11,99 @@
 
 using namespace std;
 
-string sendrcv_TCP(int mode, string strout = ""){
-	
+string sendrcv_TCP(string strout = ""){
 	#define init_tmout 15
 
 	const int portno = 8234;
-	const char host[] = "192.168.1.200";
+	const char host[] = "192.168.1.109";
 	const string OK= "\e[92m[ OK ]\e[39m",ER="\e[93m[ ER ]\e[39m";
 
-	static int tcpsoc;					//fd														
-	static struct sockaddr_in serv_addr;
+	static int tcpsoc = 0;					//fd														
+	static struct sockaddr_in serv_addr;			// sizeof(serv_addr) = 16?
 	static struct hostent *server;
+	static bool COM_OK = false;
 
 	fd_set wbuff;
 
 	struct timeval tv;
-	tv.tv_sec = 5;
-	//tv.tv_usec = 0;
+	tv.tv_sec = 15;
 	char buffer[256] = "";
 	int Er;
 
-	if(mode == 0){	// WRITE 2 SOCK.
-		FD_ZERO(&wbuff);
-		FD_SET(tcpsoc, &wbuff);
-		Er = select(tcpsoc + 1, NULL, &wbuff, NULL, &tv);				//chk if out buffer is overflowing
-		if (Er > 0) {
-			strcpy(buffer, strout.c_str());
-			Er = write(tcpsoc,buffer,strlen(buffer));
-			if (Er < 0)
-				perror("Write socket error :");		
-		}
-		else{
-			if (Er < 0) {
-				perror("Write socket error :");
-				sleep(1);
-				return "";
-			}
-		}
-	}
-	else if (mode == 1){	// READ SOCK.
-   		recv(tcpsoc, buffer, sizeof(buffer),0);
-		return buffer;
-	}
-	else if (mode == 2){	// INIT SOCK.
+	if(tcpsoc == 0){					// automatically reset,bind soc, if write failed
 		int tmout = 0;
-		bool init_cmpltd;
+		bool init_cmpltd = false;
 		do{
 			tmout++;
 			if(tmout > init_tmout){
 				cout<<"soc.init timeout!" + ER<<endl;
 				return "";
 			}
-			sleep(1);
 			tcpsoc = socket(AF_INET, SOCK_STREAM, 0);
-			fcntl(tcpsoc, F_SETFL, O_NONBLOCK);
+			fcntl(tcpsoc, F_SETFL, O_NONBLOCK);			// set nonblocking
     			if (tcpsoc < 0) {
-				perror("IN");
-				init_cmpltd = false;
+				perror("socket def :");
+				exit(0);
 			}
    			server = gethostbyname(host);
    			bzero((char *) &serv_addr, sizeof(serv_addr));
    			serv_addr.sin_family = AF_INET;
    			bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
    			serv_addr.sin_port = htons(portno);
-			connect(tcpsoc,(const sockaddr*)&serv_addr,sizeof(serv_addr));
-			if (connect(tcpsoc,(const sockaddr*)&serv_addr,sizeof(serv_addr)) < 0){
-				perror("InitEr");
-				init_cmpltd = false;
-			}
-			else
+			if(connect(tcpsoc,(const sockaddr*)&serv_addr,sizeof(serv_addr)) < 0 ){	
+        			if (errno != EINPROGRESS){
+					perror("connect() failed ");
+            			}
 				init_cmpltd = true;
+			}
+			else{
+				init_cmpltd = true;
+			}
 		}while(!init_cmpltd);
-		cout<<"socket.init completed!" + OK <<endl;
+	}
+	if(strout != ""){	// WRITE 2 SOCK.
+		FD_ZERO(&wbuff);
+		FD_SET(tcpsoc, &wbuff);
+		Er = select(tcpsoc + 1, NULL, &wbuff, NULL, &tv);			
+		if(Er <= 0){					// buffer is overflowing
+			perror("Write select error :");
+			sleep(0.2);
+			tcpsoc = 0;
+			return "";
+		}
+		strcpy(buffer, strout.c_str());
+		Er = write(tcpsoc,buffer,strlen(buffer));
+		if (Er < 0){					// error when writing
+			if(COM_OK)
+				cout<<"COM. LINK STOPPED"<<ER<<endl;
+			else{
+				perror("Connecting :\e[93m[ ER ]\e[39m");
+			}
+			COM_OK = false;	
+			tcpsoc = 0;	
+			return "";
+		}
+		else{
+			if(!COM_OK)
+				cout<<"COM. LINK STARTED"<<OK<<endl;
+			COM_OK = true;
+			cout<<"MSG SENT."<<endl;
+		}
+	}
+	else{	// READ SOCK.
+   		recv(tcpsoc, buffer, sizeof(buffer),0);
+		return buffer;
 	}
 	return "";
-
 }
 int main(){
-	sendrcv_TCP(2,"");
 	while(true){
-		sendrcv_TCP(0,"hello world");
-		string msg = sendrcv_TCP(1,"");
+		sendrcv_TCP("hello ");
+		string msg = sendrcv_TCP();
 		if(msg.length() > 0){
 			cout<<"rcvd :"<<msg;
 		}
-		sleep(1);
+		sleep(0.1);
 	}
 	return 0;
 }
